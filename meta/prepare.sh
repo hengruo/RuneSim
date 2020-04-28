65a286d239b595d1393d9353d338c20c687557a1
H. Z <7128775+setoidz@users.noreply.github.com>
2020-04-28 01:01:19 -0700

    Update code generators

--- meta/prepare.sh
+++ meta/prepare.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 wget https://dd.b.pvp.net/latest/set1-lite-en_us.zip
+wget https://dd.b.pvp.net/latest/set2-lite-en_us.zip
 unzip set1-lite-en_us.zip -d set1
+unzip set2-lite-en_us.zip -d set2
 mv set1/en_us/data/set1-en_us.json ./set1-en_us.json
-rm -rf set1
-rm set1-lite-en_us.zip
+mv set2/en_us/data/set2-en_us.json ./set2-en_us.json
+rm -rf set1 set2
+rm set1-lite-en_us.zip set2-lite-en_us.zip
